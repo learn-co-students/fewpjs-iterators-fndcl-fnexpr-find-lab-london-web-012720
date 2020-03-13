ea13843bc68fc5e547218464d0feb3049a1f671f
Amy Franz <amy.franz@gmail.com>
2020-03-13 13:50:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const testVar = {}
+const testVar = {};
 
 function testFunc() {
-  return "hi"
+  return "hi";
+}
+
+function superbowlWin(arraySB) {
+  debugger;
+  let hi = arraySB.find(function(s) {
+    return s["result"] === "W";
+  });
+  if (hi != undefined) {
+    return hi["year"];
+  } else {
+    return hi;
+  }
 }
