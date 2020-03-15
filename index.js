4aa559e845ef5d12b455f6cec1a3777de0b87044
RikBala <rik280491@googlemail.com>
2020-03-15 16:13:51 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const superbowlWin = record => {
+    let result = record.find( record => record.result === "W" )
+    if (result)
+      return result.year 
+    else 
+      return undefined
+  }
+
+
+
