ce7bd5186f0f68a6a1318383fd959c2d953ade3f
Sergio Robles <sroblesc@gmail.com>
2020-04-06 20:38:46 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = record => {
+  let result = record.find( record => record.result === "W" )
+  if (result)
+    return result.year 
+  else 
+    return undefined
+}
